0f2f971d3bdf42e6921d73fef7d8f757f25b738d
Eldor Bekpulatov <eb654@cornell.edu>
2021-09-29 23:19:41 -0400

    cleaned up the static files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # importing required modules 
-import os, math, shutil, random
+import os, random
 from flask import Flask, render_template, request, redirect, url_for
 import textract 
 
@@ -20,7 +20,11 @@ ALLOWED_EXTENSIONS = set([ "csv", "doc", "docx", "eml", "epub", "gif", "htm", "h
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    
+
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='favicon.ico'))
+
 @app.route('/')  
 def index():  
     return render_template("file_upload_form.html")  
@@ -45,7 +49,7 @@ def upload():
                     except Exception as e:
                         text = str(e)
                     content.append({ "id": file_id, "name":file.filename, "type": file.content_type, "text": text })
-                    # os.remove(file_path)
+                    os.remove(file_path)
                     file_id += 1
             return render_template("file_search_form.html", data=content)  
     else:
